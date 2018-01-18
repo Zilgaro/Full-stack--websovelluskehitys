5083cb3a664ad47ed810ec689f6fdd9b2414c5ef
Zilgaro <verner_saaristo@msn.com>
2018-01-18 15:03:15 +0200

    tehtävä 2.8

--- Viikko2/puhelinluettelo/src/App.js
+++ Viikko2/puhelinluettelo/src/App.js
@@ -6,9 +6,13 @@ class App extends React.Component {
     super(props)
     this.state = {
       persons: [
-        { name: 'Arto Hellas' }
+        {
+          name: 'Arto Hellas',
+          number: '040-2123213'
+        }
       ],
-      newName: ''
+      newName: '',
+      newNumber: ''
     }
   }
 
@@ -18,16 +22,18 @@ class App extends React.Component {
     })
   }
 
-  addName = (event) => {
+  addPerson = (event) => {
     event.preventDefault()
     const personObj = {
-      name: this.state.newName
+      name: this.state.newName,
+      number: this.state.newNumber
     }
     if (!this.checkIfNameTaken(this.state.persons, this.state.newName)) {
       const persons = this.state.persons.concat(personObj)
       this.setState({
         persons,
-        newName: ''
+        newName: '',
+        newNumber: ''
       })
     } else {
       alert("Name already taken!")
@@ -37,17 +43,25 @@ class App extends React.Component {
   handleNameChange = (event) => {
     this.setState({newName: event.target.value})
   }
+  handleNumberChange = (event) => {
+    this.setState({newNumber: event.target.value})
+  }
   render() {
     const persons = () => this.state.persons.map(p =>
-                          <Person key={p.name} name={p.name} />)
+                          <Person key={p.name} name={p.name}
+                          number={p.number} />)
 
     return (
       <div>
         <h2>Puhelinluettelo</h2>
-        <form onSubmit = {this.addName}>
+        <form onSubmit = {this.addPerson}>
           <div>
             nimi: <input value={this.state.newName}
                          onChange={this.handleNameChange}/>
+          </div>
+          <div>
+            numero: <input value={this.state.newNumber}
+                          onChange={this.handleNumberChange}/>
           </div>
           <div>
             <button type="submit">lisää</button>
