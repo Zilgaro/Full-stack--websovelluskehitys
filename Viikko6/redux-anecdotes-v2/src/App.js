7380205342e219500d600be1766302b5b92bb3b8
Zilgaro <verner_saaristo@msn.com>
2018-02-28 15:11:06 +0200

    Tehtävä 6.10

--- Viikko6/redux-anecdotes-v2/src/App.js
+++ Viikko6/redux-anecdotes-v2/src/App.js
@@ -2,7 +2,16 @@ import React from 'react'
 import ConnectedNotification from './components/Notification'
 import ConnectedAnecdoteForm from './components/AnecdoteForm'
 import ConnectedAnecdoteList from './components/AnecdoteList'
+import anecdoteService from './services/anecdotes'
+import { connect } from 'react-redux'
+import { anecdoteInitialization } from './reducers/anecdoteReducer'
+
 class App extends React.Component {
+  componentWillMount = async () => {
+    const notes = await anecdoteService.getAll()
+    this.props.anecdoteInitialization(notes)
+  }
+
   render() {
     return (
       <div>
@@ -15,4 +24,7 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default connect(
+  null,
+  { anecdoteInitialization }
+)(App)
